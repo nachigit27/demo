d9d0018f269c2a70dd74c03a5048883c17fdbdba
nachigit27 <narasimha.athani@gmail.com>
2019-07-23 12:38:21 +0300

    changes

---
+++ Hashtable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace HashtableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hashtable ht = new Hashtable();
+            
+            ht.Add("001", ".Net");
+            ht.Add("002", "C#");
+            ht.Add("003", "ASP.NET");
+
+            ICollection keys = ht.Keys;
+            
+            foreach (String k in keys)
+            {
+
+                Console.WriteLine(ht[k]);
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
